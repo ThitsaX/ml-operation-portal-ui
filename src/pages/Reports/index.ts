a0528f8a845133a4ac4a41750df8911ac888963e
Kyaw Soe <kyaw.soe@thitsaworks.com>
2025-09-17 10:42:54 +0630

    fix: removed the settlement report

--- src/pages/Reports/index.ts
+++ src/pages/Reports/index.ts
@@ -1,13 +1,11 @@
 import SettlementDetailReport from './SettlementDetailReport'
 import SettlementSummaryReport from './SettlementSummaryReport'
-import SettlementReport from './Settlement'
 import SettlementStatementReport from './SettlementStatementReport'
 import SettlementAuditReport from './SettlementAuditReport'
 
 export {
   SettlementDetailReport,
   SettlementSummaryReport,
-  SettlementReport,
   SettlementStatementReport,
   SettlementAuditReport
 }
