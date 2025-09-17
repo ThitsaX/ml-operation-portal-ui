6e34c4f5f1e5386d61fb7b92a1a8ca9a1b7e989d
Kyaw Soe <kyaw.soe@thitsaworks.com>
2025-09-17 14:02:19 +0630

    refactor: use VITE env vars for API URLs

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -1,15 +1,10 @@
 export class Configs {
-  static instance: Configs;
+  private static instance: Configs;
 
-  /**
-   * @type {boolean} - Whether the app is running in a development environment or not. Default true.
-   */
-  public static readonly __DEV__: boolean = true;
-  public static readonly AppName: string = 'Operation Portal';
-  // public readonly BASE_URL: string = 'https://dfspportal.participant.test.sanbox.wynepayhubsanbox-pre.com:444'
-  public readonly BASE_URL: string = 'http://localhost:8003';
-  // prevent new with private constructor
-  private constructor() {}
+  public readonly APP_NAME = 'Operation Portal';
+  
+  private constructor() {
+  }
 
   static getInstance(): Configs {
     if (!Configs.instance) {
--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,23 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import visualizer from "rollup-plugin-visualizer";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    tsconfigPaths(),
-    visualizer({
-      open: true,
-      gzipSize: true,
-      brotliSize: true,
-    }),
-  ],
-  server: {
-    proxy: {
-      '/public': {
-        target: 'http://localhost:8003',
-        changeOrigin: true,
-        secure: false,
-      },
-      '/secured': {
-        target: 'http://localhost:8003',
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "VITE_");
+
+  return {
+    plugins: [
+      react(),
+      tsconfigPaths(),
+      visualizer({
+        open: true,
+        gzipSize: true,
+        brotliSize: true,
+      }),
+    ],
+    define: {
+      'process.env': env
+    }
+  };
 });
