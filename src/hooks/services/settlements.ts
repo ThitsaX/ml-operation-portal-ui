afc364fc8f88ef8b50e53355b5e48feeb0825478
Zwe Htet Myat <zwehtet.myat@thitsaworks.com>
2025-10-14 14:11:50 +0630

    fix: build fail fixed

--- src/hooks/services/settlements.ts
+++ src/hooks/services/settlements.ts
@@ -1,24 +1,24 @@
-import {
-    getUserListByParticipant, getRoleListByParticipant, getOrganizationListByParticipant, getContactList, getParticipantProfile,
-    getParticipantCurrencyList, getHubCurrency, getLiquidityProfileList, getParticipantList
-} from '@services/participant'
-import { useQuery, type UseQueryOptions } from '@tanstack/react-query'
-import {
-    type IApiErrorResponse, type IParticipantUser,
-} from '@typescript/services'
-import { ISettlementWindow, IFinalizeSettlement } from '@typescript/services'
+// import {
+//     getUserListByParticipant, getRoleListByParticipant, getOrganizationListByParticipant, getContactList, getParticipantProfile,
+//     getParticipantCurrencyList, getHubCurrency, getLiquidityProfileList, getParticipantList
+// } from '@services/participant'
+// import { useQuery, type UseQueryOptions } from '@tanstack/react-query'
+// import {
+//     type IApiErrorResponse, type IParticipantUser,
+// } from '@typescript/services'
+// import { ISettlementWindow, IFinalizeSettlement } from '@typescript/services'
 import { getSettlementWindowsList, getFinalizeSettlementList } from '@services/settlements'
 
-export const useGetFinalizeSettlementList = (
-    options?: UseQueryOptions<IFinalizeSettlement[],
-        IApiErrorResponse
-    >
-) =>
-    useQuery<IFinalizeSettlement[], IApiErrorResponse>({
-        queryKey: ['getFinalizeSettlementList'],
-        queryFn: getFinalizeSettlementList,
-        ...options
-    })
+// export const useGetFinalizeSettlementList = (
+//     options?: UseQueryOptions<IFinalizeSettlement[],
+//         IApiErrorResponse
+//     >
+// ) =>
+//     useQuery<IFinalizeSettlement[], IApiErrorResponse>({
+//         queryKey: ['getFinalizeSettlementList'],
+//         queryFn: getFinalizeSettlementList,
+//         ...options
+//     })
 
 // export const useGetSettlementWindowList = (
 //     options?: UseQueryOptions<ISettlementWindow[],
